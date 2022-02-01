0bfd33c18fefae35d8d0704e59949dfc5fc97f36
Maria Eduarda Krutzsch <mariaedkru@gmail.com>
2022-02-01 13:09:06 -0300

    Tópico design patterns ajustado

---
+++ design-patterns/janeiro/projeto-escola/ESCOLA-MODEL/Avaliacao.java
@@ -0,0 +1,70 @@
+
+import java.util.*;
+
+/**
+ * @author maria
+ */
+public class Avaliacao {
+
+    /**
+     * Default constructor
+     */
+    public Avaliacao() {
+    }
+
+    /**
+     * 
+     */
+    private int cd_aluno;
+
+    /**
+     * 
+     */
+    private int cd_disciplina;
+
+    /**
+     * 
+     */
+    private int nr_avaliacao;
+
+    /**
+     * 
+     */
+    private double vlr_nota;
+
+    /**
+     * 
+     */
+    public void incluirAvaliacao() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void alterarAvaliacao() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void excluirAvaliacao() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void consultarAvaliacao() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void listarAvaliacoes() {
+        // TODO implement here
+    }
+
+}
---
+++ design-patterns/janeiro/projeto-escola/ESCOLA-MODEL/Disciplina.java
@@ -0,0 +1,78 @@
+
+import java.util.*;
+
+/**
+ * @author maria
+ */
+public class Disciplina {
+
+    /**
+     * Default constructor
+     */
+    public Disciplina() {
+    }
+
+    /**
+     * 
+     */
+    private int cd_disciplina;
+
+    /**
+     * 
+     */
+    private String nm_disciplina;
+
+    /**
+     * 
+     */
+    private String nm_professor;
+
+    /**
+     * 
+     */
+    private int qtd_avaliacoes;
+
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void incluirDisciplina() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void alterarDisciplina() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void excluirDisciplina() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void consultarDisciplina() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void listarDisciplinas() {
+        // TODO implement here
+    }
+
+}
