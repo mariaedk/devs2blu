f1e790438f7134c6e89ee1b4e0ca717940730939
Maria Eduarda Krutzsch <mariaedkru@gmail.com>
2022-02-05 13:04:40 -0300

    Finalizado back-end projeto escola

---
+++ design-patterns/projeto-escola/codigo/Escola/src/principal/MenuPrincipal.java
@@ -0,0 +1,85 @@
+package principal;
+
+import entidadeDAO.AvaliacaoDAO;
+import entidadeDAO.DisciplinaDAO;
+import entidadeDAO.MatriculaDAO;
+import entidades.Aluno;
+import entidades.Avaliacao;
+import entidades.Disciplina;
+import entidades.Matricula;
+import util.Conexao;
+
+public class MenuPrincipal 
+{
+
+	/*
+	 * TODO:
+	 * X -- AVALIACAO;
+	 * X -- MATRICULA; (salvar ok)
+	 */
+	public static void main(String[] args) 
+	{
+		Conexao.testeConexao();
+//		 Aluno
+		
+//		 cria um objeto de aluno de acordo com dados fornecidos pelo console e salva no banco de dados
+//		Aluno aluno = new Aluno().criaAluno();
+//		mesma coisa que fazer:
+//		Aluno aluno = new Aluno();
+//		aluno.criaAluno();
+		
+//		new AlunoDAO().salvaAlunoBD(aluno);
+
+//		altera os dados do aluno
+//		new AlunoDAO().alteraDadosAlunoBD();
+		
+//		exclui um aluno
+//		new AlunoDAO().excluiAlunoBD(8);
+		
+//		retorna todos os alunos isnertidos no banco de dados
+//		new AlunoDAO().retornaTodosAlunosBD();
+		
+		
+		
+		
+//		Disciplina
+		
+//		Disciplina disciplina = new Disciplina().criaDisciplina();
+		
+//		new DisciplinaDAO().salvaDisciplinaBD(disciplina);	
+		
+//		new DisciplinaDAO().alteraDadosDisciplinaBD();
+//		
+//		new DisciplinaDAO().excluiDisciplinaBD(3);
+//		
+//		new DisciplinaDAO().retornaTodasDisciplinasBD();
+		
+		
+		
+//		Matricula
+		
+//		Matricula matricula = new Matricula().criaMatricula();
+		
+//		new MatriculaDAO().salvaMatriculaBD(matricula);	
+		
+//		new MatriculaDAO().alteraDadosMatriculaBD();
+//		
+//		new MatriculaDAO().excluiMatriculaBD(3,2);
+//		
+//		new MatriculaDAO().retornaTodasMatriculasBD();
+		
+		
+//		Avaliacao
+		
+//		Avaliacao avaliacao = new Avaliacao().criaAvaliacao();
+		
+//		new AvaliacaoDAO().salvaAvaliacaoBD(avaliacao);	
+		
+//		new AvaliacaoDAO().alteraDadosAvaliacaoBD();
+//		
+//		new AvaliacaoDAO().excluiAvaliacaoBD(1,1,1);
+//		
+//		new AvaliacaoDAO().retornaTodasAvaliacoesBD();
+		
+	}
+}
